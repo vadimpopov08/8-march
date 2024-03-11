c53ac9d2c541bdce3f721faa33f9e675ab110a88
vadimpopov08 <122273142+vadimpopov08@users.noreply.github.com>
2024-03-11 15:18:02 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let congratulation = document.querySelector('.congratulation')
+let yourName = document.getElementById('yourName')
+let congratulateButton = document.getElementById('congratulateButton')
+let congratulateContainer = document.getElementsByClassName('congratulate__container')
+congratulateButton.addEventListener('click', () => {
+    congratulation.classList.add('pressed')
+    console.log(congratulation)
+    // congratulation.textContent = `Милая ${yourName.value}!`
+})
